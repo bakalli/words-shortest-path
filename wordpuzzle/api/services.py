c04629c4e220600ac98e96404b4a419e2664bd77
Jamarber Bakalli <jamarberbakalli@jamarbers-air.home>
2024-01-22 13:17:42 +0000

    Add graph building helper methods to word puzzle service

--- wordpuzzle/api/services.py
+++ wordpuzzle/api/services.py
@@ -1,9 +1,50 @@
 # api/services.py
 import asyncio
+import WordLoader from word_loader
 
 class WordPuzzleSolverService:
-    @staticmethod
+    
+    def __init__(self): 
+        self.word_loader = WorldLoader()
+        self.word_loader.load_words()
+        self.graph = {}
+    
     async def solve_puzzle(start_word, end_word): 
         #asynchronous logic for solving word puzzle
         result = []
         return result
+    
+    def build_graph(self, start_word, end_word):
+        # update self.graph with the adjacency list (node -> edges) of traversable words from start and end word 
+        nodes = [start_word, end_word]
+        discovered = set()
+        while nodes: 
+            node = nodes.pop()
+            if node not in self.graph:
+                self.graph[node] = set()
+            discovered.add(node)
+            neighbors = self.get_neighbors(node)
+            for neighbor in neighbors:
+                self.graph[node].add(neighbor)
+                 if neighbor not in discovered: nodes.append(neighbor)    
+    
+    def get_neighbors(self, word): 
+        alphabet = list(string.ascii_lowercase)
+        word_set = self.word_loader.get_word_set()
+        neighbors = set()
+        for i in range(len(word)):
+            char = word[i]
+            for letter in alphabet: 
+                if char == letter: continue 
+                possible_neighbor = word[:i] + letter + word[i+1:]
+                if possible_neighbor in word_set:
+                    neighbors.add(possible_neighbor)
+        return neighbors
+
+
+
+
+
+
+
+    
--- wordpuzzle/api/views.py
+++ wordpuzzle/api/views.py
@@ -5,23 +5,23 @@ from .word_loader import WordLoader
 from .services import WordPuzzleSolverService
 
 
-class WordPuzzleApi(APIView):
+class WordPuzzleApi(View):
     """Implement the API here"""
 
-    def get(self, request, *args, **kwargs):
+    async def get(self, request, *args, **kwargs):
         words = WordLoader.get_word_set()
 
         return HttpResponse("Implementation needed")
 
 
-    def post(self, request, *args, **kwargs):
+    async def post(self, request, *args, **kwargs):
         serializer = WordPuzzleSerializer(data=request.data)
         if serializer.is_valid():
             start_word = serializer.validated_data['startWord']
             end_word = serializer.validated_data['endWord']
 
             # Run puzzle solver
-            result = WordPuzzleSolverService.solve_puzzle(start_word, end_word)
+            result = async WordPuzzleSolverService.solve_puzzle(start_word, end_word)
 
             return Response({'result': result}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
