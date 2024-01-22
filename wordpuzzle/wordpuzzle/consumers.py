afa13f37bea6754913f626fad0d1d5a6d489bc65
Jamarber Bakalli <jamarberbakalli@jamarbers-air.home>
2024-01-22 12:10:57 +0000

    added service and consumer files for boilerplate word puzzle solver service

---
+++ wordpuzzle/wordpuzzle/consumers.py
@@ -0,0 +1,22 @@
+# wordpuzzle/consumers.py 
+import asyncio
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class WordPuzzleConsumer(AsyncWebsocketConsumer): 
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        start_word = data['startWord']
+        end_word = data['endWord']
+
+        # Perform the asynchronous operation using the service
+        result = await WordPuzzleSolverService.solve_puzzle(start_word, end_word)
+
+        # Send the result back to the WebSocket client
+        await self.send(text_data=json.dumps({'result': result}))
---
+++ wordpuzzle/wordpuzzle/services.py
@@ -0,0 +1,9 @@
+# wordpuzzle/services.py
+import asyncio
+
+class WordPuzzleSolverService:
+    @staticmethod
+    async def solve_puzzle(start_word, end_word): 
+        #asynchronous logic for solving word puzzle
+        result = []
+        return result
