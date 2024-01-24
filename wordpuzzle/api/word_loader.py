9498a0e4181414a45c462d5ab81352fa1eeed60f
Jamarber Bakalli <jamarberbakalli@Jamarbers-MacBook-Air.local>
2024-01-24 14:28:14 +0000

    Updated word loader with small and medium dictionary functionality for testing

--- wordpuzzle/api/word_loader.py
+++ wordpuzzle/api/word_loader.py
@@ -2,10 +2,34 @@ import os
 
 
 class WordLoader:
-    """Class that provides an access to the dictionary"""
+    """Class that provides an access to the dictionary.
+       Updated to also include smaller dictionaries for testing purposes.
+       Smaller dictionaries can be accessed using separate, distinct methods, 
+       to avoid confusion on which dictionary is being loaded.
+    """
 
     words = None
+    small_words = None
+    medium_words = None
 
+    @classmethod
+    def load_words_small(cls):
+        cls.small_words = set()
+        app_dir = os.path.dirname(__file__)
+        with open(app_dir + "/data/small_words.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                word = line.strip()
+                cls.words.add(word)
+                
+    @classmethod
+    def load_words_medium(cls):
+        cls.medium_words = set()
+        app_dir = os.path.dirname(__file__)
+        with open(app_dir + "/data/medium_words.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                word = line.strip()
+                cls.words.add(word)
+    
     @classmethod
     def load_words(cls):
         cls.words = set()
@@ -14,6 +38,14 @@ class WordLoader:
             for line in file:
                 word = line.strip()
                 cls.words.add(word)
+
+    @classmethod
+    def get_word_set_small(cls):
+        return cls.small_words
+
+    @classmethod
+    def get_word_set_medium(cls):
+        return cls.medium_words
 
     @classmethod
     def get_word_set(cls):
